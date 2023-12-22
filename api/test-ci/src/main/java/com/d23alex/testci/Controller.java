3cfc9d9cbb27ee39c5898f7d9ba89540078ea934
D23Alex <aleksejdormorezov@gmail.com>
2023-12-23 01:25:15 +0300

    fix attempt 16

--- api/test-ci/src/main/java/com/d23alex/testci/Controller.java
+++ api/test-ci/src/main/java/com/d23alex/testci/Controller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/api/hello")
     String hello() {
-        return "Hello!!!!!14";
+        return "Hello!!!!!16";
     }
 }
