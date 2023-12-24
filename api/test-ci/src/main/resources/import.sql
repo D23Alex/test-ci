04b1d93efc77b1ea312a697501059774d9e879cd
D23Alex <aleksejdormorezov@gmail.com>
2023-12-24 20:08:35 +0300

    fix import.sql

--- api/test-ci/src/main/resources/import.sql
+++ api/test-ci/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO Hello (id, name) VALUES (1, "HELLO1");
-INSERT INTO Hello (id, name) VALUES (2, "HELLO2");
-INSERT INTO Hello (id, name) VALUES (3, "HELLO3");
+INSERT INTO Hello (id, name) VALUES (1, 'HELLO1');
+INSERT INTO Hello (id, name) VALUES (2, 'HELLO2');
+INSERT INTO Hello (id, name) VALUES (3, 'HELLO3');
