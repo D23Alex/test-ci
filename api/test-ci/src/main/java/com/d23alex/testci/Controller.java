32800140ba1d8bc2f4d977cc0c0561fe02910c35
D23Alex <aleksejdormorezov@gmail.com>
2023-12-24 18:26:53 +0300

    new endpoint

--- api/test-ci/src/main/java/com/d23alex/testci/Controller.java
+++ api/test-ci/src/main/java/com/d23alex/testci/Controller.java
@@ -15,4 +15,9 @@ public class Controller {
     Iterable<Hello> allHellos() {
         return helloRepository.findAll();
     }
+
+    @GetMapping("/api/hello")
+    String hello() {
+        return "Hello";
+    }
 }
